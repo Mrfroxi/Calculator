73acabc62945d8459659e12795c71309c8ede182
Mrfroxi <froxi340@mail.ru>
2020-11-15 13:56:30 +0300

    Mbprog

--- DOcument.js
+++ DOcument.js
@@ -2,3 +2,17 @@
 function insert(num){
 document.querySelector('.intors').value += num;
 }
+
+function clean(){
+    document.querySelector('.intors').value = "";
+}
+
+
+
+function equal() {
+    let expresion = document.querySelector('.intors').value;
+    console.log("Привет");
+    if(expresion) {
+      document.querySelector('.intors').value = eval(document.querySelector('.intors').value);
+    }
+  }
